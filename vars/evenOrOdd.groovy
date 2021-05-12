ccd7e6bcaef72a46cd1819d9909b180254c377d0
mrlandscape20 <66933774+mrlandscape20@users.noreply.github.com>
2021-05-12 21:46:00 +0700

    Update evenOrOdd.groovy

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -1,26 +1,8 @@
 // vars/evenOrOdd.groovy
 def call(int buildNumber) {
   if (buildNumber % 2 == 0) {
-    pipeline {
-      agent any
-      stages {
-        stage('Even Stage') {
-          steps {
-            echo "The build number is even"
-          }
-        }
-      }
-    }
+    return "The build number is even"
   } else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
-            echo "The build number is odd"
-          }
-        }
-      }
-    }
+    return "The build number is odd"
   }
 }
